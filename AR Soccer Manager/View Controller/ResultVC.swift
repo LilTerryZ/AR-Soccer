5a30ad0638db80473dc3561eab0b17316f493a37
Haonan <zhanhaon@sheridancollege.ca>
2022-11-04 02:21:09 -0400

    Some minor updates in UI

---
+++ AR Soccer Manager/View Controller/ResultVC.swift
@@ -0,0 +1,24 @@
+//
+//  ResultVC.swift
+//  AR Soccer Manager
+//
+//  Created by Haonan Zhang on 2022-11-04.
+//
+
+import Foundation
+import UIKit
+
+class ResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Homebcg.jpg")!)
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+}
