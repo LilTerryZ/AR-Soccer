c4f2656855d0e7c9384da58dd0097dee8a4a9efc
Samuel Gerges <gergess@sheridancollege.ca>
2022-11-14 20:49:12 -0500

    add user default (LocalStorage)

---
+++ AR Soccer Manager/Functions/UserRepositoryUDService.swift
@@ -0,0 +1,35 @@
+//
+//  UserRepositoryUDService.swift
+//  AR Soccer Manager
+//
+//  Created by Samuel Gerges on 2022-11-14.
+//
+
+import Foundation
+class UserRepository {
+    let userDefaults: UserDefaults
+    // MARK: - Lifecycle
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+    // MARK: - API
+    func storeInfo(itemID: String, data: Data) {
+        saveValue(value: data, itemID: itemID)
+    }
+    
+    func getInfo(itemID: String) -> Data? {
+        let returnData: Data? = readValue(itemID: itemID)
+        return (returnData)
+    }
+    
+    func removeInfo(itemID: String) {
+        userDefaults.removeObject(forKey: itemID)
+    }
+    // MARK: - Private
+    private func saveValue(value: Any, itemID: String) {
+        userDefaults.set(value, forKey: itemID)
+    }
+    private func readValue<T>(itemID: String) -> T? {
+        return userDefaults.value(forKey: itemID) as? T
+    }
+}
